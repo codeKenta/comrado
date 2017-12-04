75a7e67a817f5aa287298e8cafd616e2b8f589be
Kenneth Österholm <kest1600@student.miun.se>
2017-12-04 09:13:54 +0100

    added font awesome, start budiling loggedin component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { SignInComponent } from './landing/sign-in/sign-in.component';
 import { SignUpComponent } from './landing/sign-up/sign-up.component';
+import { LoggedInComponent } from './logged-in/logged-in.component';
+import { FeedComponent } from './logged-in/feed/feed.component';
 
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 const appRoutes: Routes =
 [
   { path: 'signin', component: SignInComponent },
-  { path: 'signup', component: SignUpComponent }
-  
+  { path: 'signup', component: SignUpComponent },
 ]
 
 @NgModule({
@@ -20,11 +22,14 @@ const appRoutes: Routes =
     AppComponent,
     LandingComponent,
     SignInComponent,
-    SignUpComponent
+    SignUpComponent,
+    LoggedInComponent,
+    FeedComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
